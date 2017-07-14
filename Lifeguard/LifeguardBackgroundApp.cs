c60f2b3cc493142f635bcb2e41f1adf7b5843a18
default user <default user@DEFAULTUSER-HP>
2017-07-14 14:48:03 -0700

    Bump version for new build

--- Lifeguard/LifeguardBackgroundApp.cs
+++ Lifeguard/LifeguardBackgroundApp.cs
@@ -22,6 +22,8 @@ namespace Lifeguard
 {
     class LifeguardBackgroundApp
     {
+        private const string VERSION_NUMBER = "1.0.12.0";
+
         [DllImport("user32.dll")]
         static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
@@ -257,11 +259,13 @@ namespace Lifeguard
                 LastBitmap = (Bitmap)screenshot.Clone();
 
                 string base64String = Convert.ToBase64String(imageBytes);
+                string useragent = Environment.OSVersion.ToString() + " v" + VERSION_NUMBER + " b" + Assembly.GetEntryAssembly().GetName().Version;
                 //base64String = "/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAA8AAD/4QMxaHR0cDovL25zLmFkb2JlLmNvbS94YXAvMS4wLwA8P3hwYWNrZXQgYmVnaW49Iu+7vyIgaWQ9Ilc1TTBNcENlaGlIenJlU3pOVGN6a2M5ZCI/PiA8eDp4bXBtZXRhIHhtbG5zOng9ImFkb2JlOm5zOm1ldGEvIiB4OnhtcHRrPSJBZG9iZSBYTVAgQ29yZSA1LjYtYzEzOCA3OS4xNTk4MjQsIDIwMTYvMDkvMTQtMDE6MDk6MDEgICAgICAgICI+IDxyZGY6UkRGIHhtbG5zOnJkZj0iaHR0cDovL3d3dy53My5vcmcvMTk5OS8wMi8yMi1yZGYtc3ludGF4LW5zIyI+IDxyZGY6RGVzY3JpcHRpb24gcmRmOmFib3V0PSIiIHhtbG5zOnhtcD0iaHR0cDovL25zLmFkb2JlLmNvbS94YXAvMS4wLyIgeG1sbnM6eG1wTU09Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC9tbS8iIHhtbG5zOnN0UmVmPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvc1R5cGUvUmVzb3VyY2VSZWYjIiB4bXA6Q3JlYXRvclRvb2w9IkFkb2JlIFBob3Rvc2hvcCBDQyAyMDE3IChNYWNpbnRvc2gpIiB4bXBNTTpJbnN0YW5jZUlEPSJ4bXAuaWlkOkZFMzI4MkY5NTQ4QjExRTc5RENBRjI2NzNCOEJBMjYwIiB4bXBNTTpEb2N1bWVudElEPSJ4bXAuZGlkOkZFMzI4MkZBNTQ4QjExRTc5RENBRjI2NzNCOEJBMjYwIj4gPHhtcE1NOkRlcml2ZWRGcm9tIHN0UmVmOmluc3RhbmNlSUQ9InhtcC5paWQ6RkUzMjgyRjc1NDhCMTFFNzlEQ0FGMjY3M0I4QkEyNjAiIHN0UmVmOmRvY3VtZW50SUQ9InhtcC5kaWQ6RkUzMjgyRjg1NDhCMTFFNzlEQ0FGMjY3M0I4QkEyNjAiLz4gPC9yZGY6RGVzY3JpcHRpb24+IDwvcmRmOlJERj4gPC94OnhtcG1ldGE+IDw/eHBhY2tldCBlbmQ9InIiPz7/7gAOQWRvYmUAZMAAAAAB/9sAhAAGBAQEBQQGBQUGCQYFBgkLCAYGCAsMCgoLCgoMEAwMDAwMDBAMDg8QDw4MExMUFBMTHBsbGxwfHx8fHx8fHx8fAQcHBw0MDRgQEBgaFREVGh8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx//wAARCAAIAAgDAREAAhEBAxEB/8QASgABAAAAAAAAAAAAAAAAAAAACAEBAAAAAAAAAAAAAAAAAAAAABABAAAAAAAAAAAAAAAAAAAAABEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8AVIP/2Q==";
                 // Create the HttpContent for the form to be posted.
                 var requestContent = new FormUrlEncodedContent(new[] {
                     new KeyValuePair<string, string>("machineid", machineId),
-                    new KeyValuePair<string, string>("imagedata", base64String)
+                    new KeyValuePair<string, string>("imagedata", base64String),
+                    new KeyValuePair<string, string>("useragent", useragent),
                 });
 
                 //                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", "Token " + token);
